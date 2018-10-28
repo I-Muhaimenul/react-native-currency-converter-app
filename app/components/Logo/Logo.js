e4ade68c18b8abaf9fbed2b62179dae29f694f1a
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2018-10-28 16:38:37 +0600

    35% done

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { View, ImageBackground, Image, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, ImageBackground, Image, Text, Keyboard } from 'react-native';
 
 import styles from './styles';
 
-const Logo = () => (
-    <View style={styles.container}>
-        <ImageBackground resizeMode="contain" 
-        style={styles.containerImage} 
-        source={require('./images/background.png')} >
-            <Image resizeMode="contain" 
-            style={styles.image} 
-            source={require('./images/logo.png')} />
-        </ImageBackground>
-        <Text style={styles.text}>Currency Converter</Text>
-    </View>
-);
+class Logo extends Component {
+    componentDidMount() {
+        this.keyboardShowListener = Keyboard.addListener('keyboardWillShow', this.keyboardShow)
+        this.keyboardHideListener = Keyboard.addListener('keyboardWillHide', this.keyboardShow)
+    }
+    componentWillUnmount() {}
+    keyboardShow () {}
+    keyboardhide () {}
+    render () {
+        return (
+            <View style={styles.container}>
+                <ImageBackground resizeMode="contain" 
+                style={styles.containerImage} 
+                source={require('./images/background.png')} >
+                    <Image resizeMode="contain" 
+                    style={styles.image} 
+                    source={require('./images/logo.png')} />
+                </ImageBackground>
+                <Text style={styles.text}>Currency Converter</Text>
+            </View>
+        );
+    }
+}
 
 export default Logo;
