a021c112adfd97b102074948344e6fc5b82dae0f
Muhaimenul Islam <muhaimenulislam@iut-dhaka.edu>
2018-10-29 16:41:53 +0600

    40% done

--- app/components/Logo/styles.js
+++ app/components/Logo/styles.js
@@ -4,29 +4,28 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 const imageWidth = Dimensions.get('window').width / 2;
 
 export default EStyleSheet.create({
-    $largeContainerSize: imageWidth,
-    $largeImageSize: imageWidth / 2,
-    $smallContainerSize: imageWidth / 2,
-    $smallImageSize: imageWidth / 4,
-    container: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '$primaryBlue'
-    },
-    containerImage: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '$largeContainerSize',
-        height: '$largeContainerSize'
-    },
-    image: {
-        width: '$largeImageSize',
-    },
-    text: {
-        fontWeight: '600',
-        fontSize: 28,
-        letterSpacing: -0.5,
-        marginTop: 20,
-        color: '$white'
-    }
+  $smallContainerSize: imageWidth / 2,
+  $smallImageSize: imageWidth / 4,
+  $largeContainerSize: imageWidth,
+  $largeImageSize: imageWidth / 2,
+  container: {
+    alignItems: 'center',
+  },
+  containerImage: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '$largeContainerSize',
+    height: '$largeContainerSize',
+  },
+  logo: {
+    width: '$largeImageSize',
+    tintColor: '$primaryBlue',
+  },
+  text: {
+    color: '$white',
+    fontSize: 28,
+    letterSpacing: -0.5,
+    marginTop: 15,
+    fontWeight: '600',
+  },
 });
--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Home from './screens/Home';
+//screens
+// import Home from './screens/Home';
+import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
     $primaryBlue: '#4F6D7A',
@@ -9,6 +11,8 @@ EStyleSheet.build({
     $border: '#E2E2E2',
     $inputText: '#797979',
     $lightGray: '#F0F0F0',
+    $darkText: '#343434',
 });
 
-export default () => <Home />;
+// export default () => <Home />;
+export default () => <CurrencyList />;
